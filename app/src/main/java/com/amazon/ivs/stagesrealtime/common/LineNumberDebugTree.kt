e6c1525daf30e818d6175d5aefeb6db9f88d4127
Eddy Gorbunov <edijs.gorbunovs@testdevlab.com>
2025-06-11 17:02:00 +0300

    Updated libs;

--- app/src/main/java/com/amazon/ivs/stagesrealtime/common/LineNumberDebugTree.kt
+++ app/src/main/java/com/amazon/ivs/stagesrealtime/common/LineNumberDebugTree.kt
@@ -3,6 +3,7 @@ package com.amazon.ivs.stagesrealtime.common
 import timber.log.Timber
 
 private const val TIMBER_TAG = "StagesRT"
+
 class LineNumberDebugTree : Timber.DebugTree() {
     private var method = ""
 
